90d3a56496f9a32aa6d07e623dacac8f2dc97706
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2026-02-24 17:42:52 -0600

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -2,8 +2,8 @@ class Config:
     
     patch_size = 64
     latent_size = 16
-    batch_size = 100
+    batch_size = 120
     learning_rate = 3e-3
     data_dir = "data/fin_min"
-    epochs = 100
+    epochs = 200
     save_path = "ckpt/AE.pth"
